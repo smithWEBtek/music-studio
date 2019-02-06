9cfe11caa43a4c794219fcf8b793ce174bc07ebe
smithwebtek <brad@smithwebtek.com>
2019-02-06 06:46:30 -0500

    Various minor updates

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+
 import studentReducer from './store/reducers/studentReducer'
 import teacherReducer from './store/reducers/teacherReducer'
 import resourceReducer from './store/reducers/resourceReducer'
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,34 +1,34 @@
 export {
-	createLesson,
-	deleteLesson,
-	updateLesson,
-	fetchLessons
+  createLesson,
+  deleteLesson,
+  updateLesson,
+  fetchLessons
 } from './lessonActions'
 
 export {
-	createResource,
-	deleteResource,
-	updateResource,
-	fetchResources
+  createResource,
+  deleteResource,
+  updateResource,
+  fetchResources
 } from './resourceActions'
 
 export {
-	createStudent,
-	deleteStudent,
-	updateStudent,
-	fetchStudents
+  createStudent,
+  deleteStudent,
+  updateStudent,
+  fetchStudents
 } from './studentActions'
 
 export {
-	createTeacher,
-	deleteTeacher,
-	updateTeacher,
-	fetchTeachers
+  createTeacher,
+  deleteTeacher,
+  updateTeacher,
+  fetchTeachers
 } from './teacherActions'
 
 export {
-	createLessonResource,
-	deleteLessonResource,
-	// updateLessonResource,
-	fetchLessonResources
+  createLessonResource,
+  deleteLessonResource,
+  updateLessonResource,
+  fetchLessonResources
 } from './lessonResourceActions'
